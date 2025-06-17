4efb4f78674c97d0e3cc5cc08497bf8140eded8b
NicolasARDBE <na.reyesd@javeriana.edu.co>
2025-06-17 16:56:12 -0500

    feat: Gherkin implementation

--- src/test/java/steps/ListManagementSteps.java
+++ src/test/java/steps/ListManagementSteps.java
@@ -1,7 +1,16 @@
 package steps;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.logging.Logger;
 import com.inter2025api.facedes.ListManagementFacade;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 import com.inter2025api.context.TestContext;
 
@@ -15,4 +24,25 @@ public class ListManagementSteps {
         this.listManagementFacade = listManagementFacade;
     }
 
+    @Given("a new list is created")
+    public void a_new_list_is_created() {
+        // Assuming ListManagementFacade creates a new list on instantiation or has a reset method
+        logger.info("A new list is created.");
+        // If needed: listManagementFacade.resetList();
+    }
+
+    @When("items are added to the list")
+    public void items_are_added_to_the_list() {
+        logger.info("Items added to the list: ");
+    }
+
+    @Then("the list should contain the added items")
+    public void the_list_should_contain_the_added_items() {
+        logger.info("List contains the added items.");
+    }
+
+    @And("the list should be retrievable")
+    public void the_list_should_be_retrievable() {
+        logger.info("List is retrievable: ");
+    }
 }
